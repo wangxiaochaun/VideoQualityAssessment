ed51f62e10b095459b3265843c720aecf73f8b26
Xiaochuan <wangxiaochaun@163.com>
2018-07-11 21:12:09 +0800

    Initilization

---
+++ demo.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cmath>
+#include <opencv2\opencv.hpp>
+#include "PFM.h"
+#include <string>
+#include <vector>
+#include <time.h>
+#include "hole_filling.h"
+
+using namespace std;
+
+int main()
+{
+	cv::Mat I_ref = cv::imread("texture.bmp");
+	cv::Mat D_ref = cv::imread("depth.bmp");
+	cv::cvtColor(D_ref, D_ref, CV_RGB2GRAY);
+
+	cv::Mat I_syn = cv::Mat(I_ref.size().height, I_ref.size().width, CV_8UC3);
+	cv::Mat D_syn = cv::Mat(I_ref.size().height, I_ref.size().width, CV_8U);
+
+	//cv::imshow("I_ref", D_ref);
+
+	algorithm test;
+
+	clock_t start = clock();
+
+	test.Fehn_interpolation(I_ref, D_ref, I_syn, D_syn);
+
+	clock_t ends = clock();
+
+	std::cout << "Running time of 3D warping (ms): " << static_cast<double>(ends - start) / CLOCKS_PER_SEC * 1000 << std::endl;
+	
+
+	cv::waitKey(0);
+}
---
+++ hole_filling.cpp
@@ -0,0 +1,79 @@
+#include "hole_filling.h"
+#include <time.h>
+#include <vector>
+
+bool algorithm::Fehn_interpolation(const cv::Mat & I_ref, const cv::Mat & D_ref, cv::Mat & I_syn, cv::Mat & D_syn)
+{
+	// 这里改变了传入参考视点深度图像的类型（前提是深度图是单通道的）
+
+	cv::GaussianBlur(D_ref, D_ref, cv::Size(3, 3), 0, 0);
+
+	warping_1d(I_ref, D_ref, I_syn, D_syn);
+
+	// 裁剪掉边框
+	int s = 25; // 默认裁剪掉的边的宽度
+	cv::Rect myROI(s, 0, I_syn.size().width - 2 * s, I_syn.size().height);
+
+	cv::Mat mask;
+
+	I_syn(myROI).copyTo(mask);
+
+	cv::resize(mask, mask, I_syn.size(), 0, 0, cv::INTER_NEAREST);
+
+	cv::medianBlur(mask, mask, 3);
+	cv::imshow("mask", mask);
+
+	mask.copyTo(I_syn);
+
+	return true;
+}
+
+bool algorithm::warping_1d(const cv::Mat & I_ref, const cv::Mat & D_ref, cv::Mat & I_syn, cv::Mat & D_syn)
+{
+	// 输入深度图必须是单通道的，因为我不会写通道检查
+
+	std::vector<int> table(256);
+
+	int knear = 0;
+	int kfar = 128;
+	int eye_seperation = 6;
+	int view_distance = 800;
+	int Npix = 320;
+	int S = 25;
+	cv::Size size = I_ref.size();
+	//cout << size.height << " " << size.width << endl;
+	float A, h;
+
+	for (int i = 0; i < 256; i++)
+	{
+		A = i * (knear / 64 + kfar / 16) / 255.0;
+		h = -eye_seperation * Npix * (A - kfar / 16) / view_distance;
+		table[i] = static_cast<int>(h / 2);
+	}
+
+	//for (auto x : table)
+	//{
+	//	cout << x << " ";
+	//}
+
+	int depth_level;
+	int shift;
+
+	for (int i = 0; i < size.height; i++)
+		for (int j = 0; j < size.width; j++)
+		{
+			depth_level = D_ref.at<uchar>(i, j);
+			shift = table[depth_level];
+			if (j + shift - S >= 0 && j + shift - S < size.width)
+			{
+				I_syn.at<cv::Vec3b>(i, j + shift - S) = I_ref.at<cv::Vec3b>(i, j);
+				D_syn.at<uchar>(i, j + shift - S) = D_ref.at<uchar>(i, j);
+			}
+		}
+
+	// test
+	cv::imshow("texture", I_syn);
+	//cv::imshow("depth", D_syn);
+
+	return true;
+}
